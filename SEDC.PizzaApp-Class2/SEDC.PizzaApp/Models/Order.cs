adb311fa44039e92ea0b88b52c43470c0afbf6f5
Manass2 <57198394+Manass2@users.noreply.github.com>
2020-09-04 02:19:25 +0200

    homework class 2

---
+++ SEDC.PizzaApp-Class2/SEDC.PizzaApp/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Order() { }
+        public Order(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
