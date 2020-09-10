f7f15d0ccdb4a096a933cf3666f1f9e91b7d5011
Manass2 <57198394+Manass2@users.noreply.github.com>
2020-09-10 20:42:29 +0200

    class 3 homework

---
+++ class 3 homework/SEDC.PizzaApp/SEDC.PizzaApp/Models/Mappers/PizzaMapper.cs
@@ -0,0 +1,39 @@
+﻿using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Models.Mappers
+{
+    public static class PizzaMapper
+    {
+
+        public static PizzaViewModel PizzaToViewModel(this Pizza pizza)
+        {
+
+            if(pizza.HasExtras)
+            {
+                return new PizzaViewModel
+                {
+                    Id = pizza.Id,
+                    Name = pizza.Name,
+                    Price = pizza.Price += 10,
+                    PizzaSize = pizza.PizzaSize
+                };
+            } 
+            else
+            {
+                return new PizzaViewModel
+                {
+                    Id = pizza.Id,
+                    Name = pizza.Name,
+                    Price = pizza.Price,
+                    PizzaSize = pizza.PizzaSize
+                };
+            }         
+
+        }
+    }
+}
