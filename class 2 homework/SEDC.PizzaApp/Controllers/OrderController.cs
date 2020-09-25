9ba9fd078a26d1f6a4b773fa43e1c1a8f92fcdc3
Manass2 <57198394+Manass2@users.noreply.github.com>
2020-09-25 13:17:31 +0200

    update

---
+++ class 2 homework/SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        [Route("Orders")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id != null)
+            {
+                Order order = OrderDB.Orders.FirstOrDefault(x => x.Id == id);
+                return View(order);
+            }
+            return new EmptyResult();
+        }
+
+        public IActionResult JsonData()
+        {
+            Order order = new Order
+            {
+                Id = 1,
+                Name = "Capri"
+            };
+            return new JsonResult(order);
+        }
+
+        public IActionResult BackToHomePage()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
